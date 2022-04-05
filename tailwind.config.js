8c20dbd32ceec85b29d1e1a59bb7885e2a2b3de3
jvtipaymerantes <61035377+jvtipaymerantes@users.noreply.github.com>
2022-04-05 17:21:50 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'black': '#000000',
+      'navbargreen': '#1DBC60',
+      'buttongreen': '#00C853',
+      'footer': '#292929',
+      'flatgreen': '#8EFFC1',
+      'darkgreen': '#007E4E',
+      'lightgray': '#F7F7F7',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
